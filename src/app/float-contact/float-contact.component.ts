160d841376c184698e2aaf52b25d1d14a18a85a0
uragana <ucha.janashia@geolab.edu.ge>
2024-04-20 16:03:16 +0400

    folating gaketda
    iconebi rcheba rogorc sachiroa ise

--- src/app/float-contact/float-contact.component.ts
+++ src/app/float-contact/float-contact.component.ts
@@ -11,17 +11,25 @@ export class FloatContactComponent {
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(e: Event) {
     let element = document.querySelector('.floatingmess') as HTMLElement;
-    if (element) {
+    let element1 = document.querySelector('.floating') as HTMLElement;
+
+    if (element && element1) {
       let scrollPosition = window.pageYOffset;
       let windowHeight = window.innerHeight;
       let bodyHeight = document.body.clientHeight;
-  
-      // Calculate the scroll percentage
+      
       let scrollPercentage = (scrollPosition / (bodyHeight - windowHeight)) * 100;
   
-      // Adjust the top property based on the scroll percentage
-      element.style.top = Math.min(scrollPercentage, 90) + '%';
+      if (scrollPercentage < 95) {
+        element.style.top = '55%';
+        element1.style.top = '55%'
+      } else {
+        element.style.top = '40%';
+        element1.style.top = '40%';
+
+      }
     }
   }
+  
   
 }
